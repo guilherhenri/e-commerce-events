4fbc3c275bacb10083b4df854879f924a3829153
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-14 11:10:11 -0300

    feat(database): define Analytics entity
    - Creates `Analytics` TypeORM entity with `eventType`, `orderId`,
    `userEmail`, `action`, and `metadata` fields.
    - Includes `AnalyticsEventType` enum for event types and defines relations.

---
+++ src/infra/entities/analytics.ts
@@ -0,0 +1,51 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ForeignKey,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+import { Order } from './order'
+
+export enum AnalyticsEventType {
+  USER_ACTION = 'user_action',
+  CONVERSION = 'conversion',
+}
+
+@Entity({ name: 'analytics' })
+export class Analytics {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({
+    type: 'enum',
+    enum: AnalyticsEventType,
+  })
+  eventType: AnalyticsEventType
+
+  @Column('uuid', { name: 'order_id', nullable: true })
+  @ForeignKey<Order>('Order', { onDelete: 'SET NULL', onUpdate: 'CASCADE' })
+  orderId: string | null
+
+  @Column('varchar', { name: 'user_email', unique: true, nullable: true })
+  userEmail: string | null
+
+  @Column('varchar')
+  action: string
+
+  @Column({
+    type: 'jsonb',
+    array: false,
+  })
+  metadata: Record<string, unknown>
+
+  @ManyToOne(() => Order, (order) => order.notifications)
+  @JoinColumn()
+  order: Order
+
+  @CreateDateColumn()
+  createdAt: Date
+}

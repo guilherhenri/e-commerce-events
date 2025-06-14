e16c2414ed21c77d5ea84da1fb7ec57675b18c48
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-14 08:38:27 -0300

    ci(commitlint): add ignore to release commits

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -20,4 +20,8 @@ export default {
     'subject-max-length': [2, 'always', 50],
     'scope-case': [2, 'always', 'lower-case'],
   },
+  ignores: [
+    (message) =>
+      message.includes('[skip ci]') || message.includes('chore(release)'),
+  ],
 }
